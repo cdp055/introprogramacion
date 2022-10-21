31c17bedadabf8111fedcdbf7a801cd09aa0454d
Felipe Jimenez <davidpiedra>
2022-10-21 17:51:43 -0500

    Virtualizacion Charla

--- src/Virtualizacion.java
+++ src/Virtualizacion.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Virtualizacion {
+public class Virtualizacion {
+    public static void main(String[] args) {
+        System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVECIONES");
+        System.out.println("Realidad virtual");
+        System.out.println("Creacion de filtros");
+        System.out.println("Juego de aprendizaje con figuras ");
+        System.out.println("Ambiente inmersivo para el estudio de vocavulario en tres idiomas con el uso de imagenes 360");
+        System.out.println("Creacion de la tecnologia gamificada para promover y concientizar la historia de la organizacion internacional juventud idente");
+        System.out.println("Creacion de dibujos animatronicos");
+        System.out.println("Creacion de filtros animados para fotos atravez del instagram");
+
+    }
 }
